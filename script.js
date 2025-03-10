d0d215436573f37c512386d0a797d17f6da2fb02
Leul Ayfokru <abateisking@gmail.com>
2025-03-10 11:02:11 +0000

    messgae finished

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function () {
     emailjs.init("2avoY-qmVBDKXT0c5"); // Your EmailJS API key
 })();
-
+document.getElementById("status").style.display = "none";
 // Handle the form submission
 document.getElementById("contact-form").addEventListener("submit", function (event) {
     event.preventDefault();
@@ -9,9 +9,11 @@ document.getElementById("contact-form").addEventListener("submit", function (eve
     // Send the email
     emailjs.sendForm("service_oxja8li", "template_u290nuf", this).then(
         function (response) {
+            document.getElementById("status").style.display = "flex";
             document.getElementById("status").innerHTML = "Your message has been sent successfully. We'll get back to you soon!";
         },
         function (error) {
+            document.getElementById("status").style.display = "flex";
             document.getElementById("status").innerHTML = "Failed to send message.";
         }
     );
