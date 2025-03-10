5b6b6859e48ac2999649e08dd58172aaea4f8b42
Leul Ayfokru <abateisking@gmail.com>
2025-03-10 02:31:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 (function () {
     emailjs.init("2avoY-qmVBDKXTOc5"); // Your EmailJS API key
-  })();
+})();
 
-  // Handle the form submission
-  document.getElementById("contact-form").addEventListener("submit", function (event) {
+// Handle the form submission
+document.getElementById("contact-form").addEventListener("submit", function (event) {
     event.preventDefault();
 
     // Send the email
-    emailjs.sendForm("service_oxja8li", "_ejs-test-maill-service_", this).then(
+    emailjs.sendForm("service_oxja8li", "template_u290nuf", this).then(
       function (response) {
         document.getElementById("status").innerHTML = "Message sent successfully!";
       },
@@ -15,5 +15,4 @@
         document.getElementById("status").innerHTML = "Failed to send message.";
       }
     );
-    emailjs.sendForm()
-  });
+});
