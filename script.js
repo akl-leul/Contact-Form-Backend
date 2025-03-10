ff1cce82d7eb9f875e0f8a76871f8c1d687bc713
Leul Ayfokru <abateisking@gmail.com>
2025-03-10 09:57:51 +0000

    all files work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-    emailjs.init("2avoY-qmVBDKXTOc5"); // Your EmailJS API key
+    emailjs.init("2avoY-qmVBDKXT0c5"); // Your EmailJS API key
 })();
 
 // Handle the form submission
@@ -8,11 +8,11 @@ document.getElementById("contact-form").addEventListener("submit", function (eve
 
     // Send the email
     emailjs.sendForm("service_oxja8li", "template_u290nuf", this).then(
-      function (response) {
-        document.getElementById("status").innerHTML = "Message sent successfully!";
-      },
-      function (error) {
-        document.getElementById("status").innerHTML = "Failed to send message.";
-      }
+        function (response) {
+            document.getElementById("status").innerHTML = "Your message has been sent successfully. We'll get back to you soon!";
+        },
+        function (error) {
+            document.getElementById("status").innerHTML = "Failed to send message.";
+        }
     );
 });
